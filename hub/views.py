e1a985fdead93d828fd2b3079b8dcc05f41c5842
zchmielewska <zchmielewska@gmail.com>
2024-10-10 20:00:42 +0200

    add CahFlowModel model

--- hub/views.py
+++ hub/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import CashFlowModel
 
 
 def models_view(request):
-    return render(request, 'models.html')
+    cashflow_models = CashFlowModel.objects.all()
+    return render(request, 'models.html', {'cashflow_models': cashflow_models})
 
 
 def runs_view(request):
