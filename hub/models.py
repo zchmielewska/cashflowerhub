eed845ba449c76169fadda8b8c53ffb63a1f057f
zchmielewska <zchmielewska@gmail.com>
2024-10-10 14:11:38 +0200

    add CashFlowModel model

--- hub/models.py
+++ hub/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class CashFlowModel(models.Model):
+    name = models.CharField(max_length=100)
+    repository_url = models.URLField()
+    short_description = models.TextField()
+
+    def __str__(self):
+        return self.name
