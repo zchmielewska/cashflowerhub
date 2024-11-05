3c61f6a2ac1c0a1dcd6dd61db365de4e2f1c762e
zchmielewska <zchmielewska@gmail.com>
2024-11-05 19:49:58 +0100

    finalize crud for model and document

--- hub/urls.py
+++ hub/urls.py
@@ -2,17 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.model_view, name='home'),
-    path('model', views.model_view, name='model'),
+    path('', views.ModelListView.as_view(), name='home'),
+
+    # Model
+    path('model', views.ModelListView.as_view(), name='model_list'),
+    path('model/add/', views.ModelCreateView.as_view(), name='model_add'),
     path('model/<int:pk>/', views.ModelDetailView.as_view(), name='model_detail'),
-    path('model/add', views.model_add, name='model_add'),
-    path('models/edit/<int:model_id>/', views.model_edit, name='model_edit'),
-    path('model/delete/<int:model_id>/', views.model_delete, name='model_delete'),
+    path('model/edit/<int:pk>/', views.ModelUpdateView.as_view(), name='model_edit'),
+    path('model/delete/<int:pk>/', views.ModelDeleteView.as_view(), name='model_delete'),
+
+    # Run
     path('run', views.run_view, name='run'),
-    path('document', views.document_view, name='document'),
-    path('document/add', views.document_add, name='document_add'),
-    path('documents/', views.DocumentListView.as_view(), name='document_list'),
+
+    # Document
+    path('document/', views.DocumentListView.as_view(), name='document_list'),
+    path('document/add', views.DocumentCreateView.as_view(), name='document_add'),
     path('document/<int:pk>/', views.DocumentDetailView.as_view(), name='document_detail'),
-    path('document/edit/<int:pk>/', views.DocumentEditView.as_view(), name='document_edit'),
+    path('document/edit/<int:pk>/', views.DocumentUpdateView.as_view(), name='document_edit'),
     path('document/delete/<int:pk>/', views.DocumentDeleteView.as_view(), name='document_delete'),
 ]
--- hub/views.py
+++ hub/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import DeleteView, DetailView, ListView, UpdateView
+from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import CashFlowModel, Document, Run
 from .forms import CashFlowModelForm, DocumentForm
 
 
-def model_view(request):
-    cashflow_models = CashFlowModel.objects.all().order_by('-id')
-    return render(request, 'model.html', {'cashflow_models': cashflow_models})
+class ModelListView(ListView):
+    model = CashFlowModel
+    template_name = 'model_list.html'
+    context_object_name = 'models'
+    ordering = ['-id']
 
 
 class ModelDetailView(DetailView):
@@ -17,36 +19,24 @@ class ModelDetailView(DetailView):
     context_object_name = 'model'
 
 
-def model_add(request):
-    if request.method == 'POST':
-        form = CashFlowModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('model')
-    else:
-        form = CashFlowModelForm()
-    return render(request, 'model_add.html', {'form': form})
+class ModelCreateView(CreateView):
+    model = CashFlowModel
+    form_class = CashFlowModelForm
+    template_name = 'model_add.html'
+    success_url = reverse_lazy('model_list')
 
 
-def model_edit(request, model_id):
-    model = get_object_or_404(CashFlowModel, pk=model_id)
-    if request.method == 'POST':
-        form = CashFlowModelForm(request.POST, instance=model)
-        if form.is_valid():
-            form.save()
-            return redirect('model')
-    else:
-        form = CashFlowModelForm(instance=model)
-
-    return render(request, 'model_edit.html', {'form': form, 'model': model})
+class ModelUpdateView(UpdateView):
+    model = CashFlowModel
+    form_class = CashFlowModelForm
+    template_name = 'model_edit.html'
+    success_url = reverse_lazy('model_list')
 
 
-def model_delete(request, model_id):
-    model = get_object_or_404(CashFlowModel, pk=model_id)
-    if request.method == 'POST':
-        model.delete()
-        return redirect('model')
-    return render(request, 'model_delete.html', {'model': model})
+class ModelDeleteView(DeleteView):
+    model = CashFlowModel
+    template_name = 'model_delete.html'
+    success_url = reverse_lazy('model_list')
 
 
 def run_view(request):
@@ -54,20 +44,17 @@ def run_view(request):
     return render(request, 'run.html', {'runs': runs})
 
 
-def document_view(request):
-    documents = Document.objects.all().order_by('-id')
-    return render(request, 'document.html', {'documents': documents})
+class DocumentListView(ListView):
+    model = Document
+    template_name = 'document_list.html'
+    context_object_name = 'documents'
 
 
-def document_add(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('document')
-    else:
-        form = DocumentForm()
-    return render(request, 'document_add.html', {'form': form})
+class DocumentCreateView(CreateView):
+    model = Document
+    form_class = DocumentForm
+    template_name = 'document_add.html'
+    success_url = reverse_lazy('document_list')
 
 
 class DocumentDetailView(DetailView):
@@ -76,20 +63,14 @@ class DocumentDetailView(DetailView):
     context_object_name = 'document'
 
 
-class DocumentEditView(UpdateView):
+class DocumentUpdateView(UpdateView):
     model = Document
     template_name = 'document_edit.html'
     fields = ['name', 'file', 'cash_flow_models']
-    success_url = reverse_lazy('document')
+    success_url = reverse_lazy('document_list')
 
 
 class DocumentDeleteView(DeleteView):
     model = Document
     template_name = 'document_delete.html'
-    success_url = reverse_lazy('document')
-
-# You also need a ListView for a list of documents
-class DocumentListView(ListView):
-    model = Document
-    template_name = 'document_list.html'
-    context_object_name = 'documents'
+    success_url = reverse_lazy('document_list')
