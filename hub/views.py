44282410508a13feb77b4d9d29d4f789fc38234f
zchmielewska <zchmielewska@gmail.com>
2024-12-19 20:42:15 +0100

    run queue for local execution

--- hub/views.py
+++ hub/views.py
@@ -8,8 +8,22 @@ from django.urls import reverse_lazy
 from queue import Queue
 from threading import Thread
 
-from .models import CashFlowModel, Document, Run
 from .forms import CashFlowModelForm, DocumentForm, RunForm
+from .models import CashFlowModel, Document, Run
+from .utils import process_run
+
+RUN_QUEUE = Queue()
+
+
+def process_run_queue():
+    while True:
+        run = RUN_QUEUE.get()
+        process_run(run)
+        RUN_QUEUE.task_done()
+
+
+worker_thread = Thread(target=process_run_queue, daemon=True)
+worker_thread.start()
 
 
 class ModelListView(ListView):
@@ -45,47 +59,11 @@ class ModelDeleteView(DeleteView):
     success_url = reverse_lazy('model_list')
 
 
-@csrf_exempt
-def get_runs_status(request):
-    runs = Run.objects.all().values('id', 'status', 'cash_flow_model__name', 'cash_flow_model__id', 'version')
-    return JsonResponse({'runs': list(runs)})
-
-
 class RunListView(ListView):
     model = Run
     template_name = 'run_list.html'
     context_object_name = 'runs'
     ordering = ['-id']
-
-
-run_queue = Queue()
-
-
-def process_run_queue():
-    """Worker thread to process runs in the queue."""
-    while True:
-        run = run_queue.get()  # Get a Run object from the queue
-        try:
-            run.status = 'running'
-            run.save()
-
-            result = subprocess.run(["python", "run.py"], capture_output=True, text=True)
-            if result.returncode == 0:
-                run.status = 'completed'
-            else:
-                run.status = 'error'
-
-        except Exception as e:
-            run.status = 'error'
-            print(f"Error processing run {run.id}: {e}")
-
-        finally:
-            run.save()
-            run_queue.task_done()
-
-
-worker_thread = Thread(target=process_run_queue, daemon=True)
-worker_thread.start()
 
 
 class RunCreateView(CreateView):
@@ -96,7 +74,7 @@ class RunCreateView(CreateView):
 
     def form_valid(self, form):
         run = form.save()
-        run_queue.put(run)
+        RUN_QUEUE.put(run)
         return redirect(self.success_url)
 
 
@@ -142,3 +120,9 @@ class DocumentDeleteView(DeleteView):
     model = Document
     template_name = 'document_delete.html'
     success_url = reverse_lazy('document_list')
+
+
+@csrf_exempt
+def get_runs_status(request):
+    runs = Run.objects.all().values('id', 'status', 'cash_flow_model__name', 'cash_flow_model__id', 'version')
+    return JsonResponse({'runs': list(runs)})
